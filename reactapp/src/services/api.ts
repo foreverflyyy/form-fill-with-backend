d2dcfda5847e66cb3d4a7138053bef26a84b5331
Nikita <nikita.ushackow.02@gmail.com>
2023-08-20 20:19:06 +0200

    exchange http on https

--- reactapp/src/services/api.ts
+++ reactapp/src/services/api.ts
@@ -1,4 +1,4 @@
-export const SERVER_PATH = "http://foreverflyyy-001-site1.ctempurl.com";
+export const SERVER_PATH = "https://foreverflyyy-001-site1.ctempurl.com";
 
 export const API_CREATE_REQUISITE = "Requisites/createRequisite";
 export const API_DATA_BY_INN = "Requisites/getDataByInn";
